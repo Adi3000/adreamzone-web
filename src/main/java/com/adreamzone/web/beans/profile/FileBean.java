0e5d35306abe5ba48b21b4f8512ef0e5d21a4487
adi <adi3000@free.fr>
2012-07-31 22:18:55 +0200

    #26 Updating some references on project #5 Changing pom for Nexus #9 First step : adding multiple file

--- src/main/java/com/adreamzone/web/beans/profile/FileBean.java
+++ src/main/java/com/adreamzone/web/beans/profile/FileBean.java
@@ -1,10 +1,12 @@
 package com.adreamzone.web.beans.profile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.servlet.annotation.MultipartConfig;
 
 import com.adreamzone.common.database.data.file.File;
 
@@ -12,29 +14,57 @@ import com.adreamzone.common.database.data.file.File;
 @RequestScoped
 public class FileBean {
 	
-	@ManagedProperty(value="#{userBean}")
+	@ManagedProperty("#{userBean}")
 	private UserBean userBean;
+
 	private List<File> fileList;
+	private java.io.File file;
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7277771912736160425L;
-	
+
 	public String uploadFiles(){
-		return "editUploadedFile";
+		return "editUploadedFiles";
 	}
 	
 	public List<File> getFileList() {
+		if(fileList == null){
+			fileList = new ArrayList<File>();
+			fileList.add(new File());
+		}
 		return fileList;
+	}
+	
+	public java.io.File getFile(int index) {
+		return fileList.get(index).getFile();
 	}
 	
 	public void setFileList(List<File> fileList) {
 		this.fileList = fileList;
 	}
 	
+	public UserBean getUserBean() {
+		return userBean;
+	}
 	
-	
-	
+	public void setUserBean(UserBean userBean) {
+		this.userBean = userBean;
+	}
+    public java.io.File getFile() {
+        return file;
+    }
+    
+	public void setFile(java.io.File file) {
+		this.file = file;
+		File newFile = new File();
+		if(fileList == null){
+			fileList = new ArrayList<File>();
+		}
+		newFile.setFile(file);
+		fileList.add(newFile);
+	}
+
+
+	public void addFileToUpload(){
+		fileList.add(new File());
+	}
 	
 }
